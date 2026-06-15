3a83f7502a6e157178d58dc28f62eb751504c801
Dawn <54471703+nymanyim@users.noreply.github.com>
2026-06-15 11:08:52 +0800

    优化：迁移电源消息拦截逻辑到WindowExtPlugin
    迁移PBT_APMSUSPEND和PBT_APMRESUMESUSPEND事件，确保高解耦

--- plugins/window_ext/lib/window_ext_listener.dart
+++ plugins/window_ext/lib/window_ext_listener.dart
@@ -2,4 +2,10 @@ abstract mixin class WindowExtListener {
   void onTaskbarCreated() {}
 
   void onShouldTerminate() {}
+
+  /// System is about to enter sleep/hibernate.
+  void onPowerSuspend() {}
+
+  /// System has resumed from sleep/hibernate.
+  void onPowerResume() {}
 }
